eba09276b6bf6d1c3354dbe5fc14e04c1dc58ef9
Casey Caruso <caseycaruso@PVC-SF-CC-MBP-2.local>
2022-11-19 14:37:42 -0800

    cleaning up functions

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# GET NPM DOWNLOADS (as a csv)
-This script allows you to pull downloads for any NPM package over any period of time, as a csv. 
+# get npm stats (as a csv)
+This script allows you to pull downloads for any npm package over time, as a csv. 
 
-There's a lot of sites to get pre-charted npm stats (e.g. http://npm-stats.org/, https://npm-stat.com/) but I couldn't find a super easy way (for my non-technical friends) to get downloads as a csv so they can pop them into Google sheets or excel. Hopefully this script fills that gap. 
+There's a lot of websites to chart npm stats (e.g. http://npm-stats.org/, https://npm-stat.com/) but I couldn't find a easy way to pull downloads <b>as a csv</b> in an extensible way.
 
-
-## HOW TO USE 
-1. clone the repo
+## how to use
+1. clone repo
 2. `npm install`
-3. in terminal, run the node script specifying your npm_package, the start date and end end date as `{start_date}:{end_date}`
+3. in terminal, run the node script specifying your npm_package, the start date, and end end date separated by a space
 
 
-Here is an example: `node getnpmstats.js web3 2020-01-01:2022-11-18`
+Here is an example: `node getnpmstats.js web3 2022-01-01 2022-11-18`
 
+## limitations
+The npm API endpoint I'm using is limited to at most <b>18 months of data</b>. Also, the earliest date data will be returned for is January 10, 2015. 
 
+## questions
 If you have any problems, you can DM me on twitter at [@caseycaruso](https://twitter.com/caseykcaruso)
--- getnpmstats.js
+++ getnpmstats.js
@@ -2,26 +2,28 @@ import axios from 'axios'
 import figlet from 'figlet'
 import { createObjectCsvWriter } from 'csv-writer'
 
-const getStats = async() => {
+const getInputs = async() => {
   let time_period = 'last-year'
   let npm_package = 'web3'
-  if (process.argv[2]) { 
-    npm_package = process.argv[2] 
-  }
+
+  if (process.argv[2]) { npm_package = process.argv[2] }
   if (process.argv[3] && process.argv[4]) {
     time_period = `${process.argv[3]}:${process.argv[4]}`
   }
+  return {'npm_package': npm_package, 'time_period': time_period}
+}
 
-  const res = await axios.get(`https://api.npmjs.org/downloads/range/${time_period}/${npm_package}`).catch((err) => {
+const getStats = async(inputs) => {
+  const res = await axios.get(`https://api.npmjs.org/downloads/range/${inputs.time_period}/${inputs.npm_package}`).catch((err) => {
     console.error("API error:", err)
     return null
-  });
+  })
   return res.data
 }
 
 const writeStats = async (statsData) => {
-  console.log(`Building you a csv of downloads for ${statsData.package} over ${statsData.start}:${statsData.end}.`)
-  figlet('Follow the devs', function(err, data) { console.log(data) })
+  console.log(`\nBuilding csv of downloads for ${statsData.package} over ${statsData.start}:${statsData.end}.`)
+  figlet('Follow the devs\n', function(err, data) { console.log(data) })
 
   const csvWriter = createObjectCsvWriter({
     path: `./npm_stats_${statsData.package}.csv`,
@@ -34,7 +36,8 @@ const writeStats = async (statsData) => {
 }
 
 const getNPMStats = async () => {
-  const res = await getStats()
+  const inputs = await getInputs()
+  const res = await getStats(inputs)
   writeStats(res)
 }
 
